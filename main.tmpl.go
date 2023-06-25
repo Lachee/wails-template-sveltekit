54ee7987e6a49e31807bea691a0ca7a402d49ae7
Lachee <lachlan.h@hotmail.com>
2023-06-25 14:35:38 +1000

    Converted prroject to template

--- main.tmpl.go
+++ main.tmpl.go
@@ -21,7 +21,7 @@ func main() {
 
 	// Create application with options
 	err := wails.Run(&options.App{
-		Title:  "Test Application",
+		Title:  "{{.ProjectName}}",
 		Width:  1024,
 		Height: 768,
 		AssetServer: &assetserver.Options{
